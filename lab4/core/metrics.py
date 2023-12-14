6eb6eb032afcd91c85d6c4eca78cc2216dfb73e7
Eliseev Vadim <eliseevv02@mail.ru>
2023-12-14 14:38:04 +0300

    chore: add r2 score evaluation

--- lab4/core/metrics.py
+++ lab4/core/metrics.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
 
 from core.model import AdvancedLinearRegression
 
@@ -8,7 +9,8 @@ def evaluate_model(model: AdvancedLinearRegression, test_data, ground_truth):
     predictions = model.predict(test_data)
     mse = mean_squared_error(ground_truth, predictions)
     mae = mean_absolute_error(ground_truth, predictions)
-    return {'MSE': mse, 'MAE': mae}
+    r2 = r2_score(ground_truth, predictions)
+    return {'MSE': mse, 'MAE': mae, 'R2': r2}
 
 
 def calculate_mse(model: AdvancedLinearRegression, test_data, ground_truth):
@@ -21,3 +23,9 @@ def calculate_mae(model: AdvancedLinearRegression, test_data, ground_truth):
     predictions = model.predict(test_data)
     mae = mean_absolute_error(ground_truth, predictions)
     return mae
+
+
+def calculate_r2_score(model: AdvancedLinearRegression, test_data, ground_truth):
+    predictions = model.predict(test_data)
+    r2 = r2_score(ground_truth, predictions)
+    return r2
